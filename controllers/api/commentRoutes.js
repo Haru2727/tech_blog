af06fd7ff78983b3bb72290607faf16bbe347b0d
Arash Saeedi <saeediarash84@gmail.com>
2021-05-19 02:11:12 -0400

    trying to complete this based off Eimans

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -3,13 +3,21 @@ const { Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 router.get('/', (req, res) => {
-    Comment.findAll({})
-      .then(dbCommentData => res.json(dbCommentData))
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
+ try {
+  const viewAllComments = Comment.findAll();
+  if (!viewAllComments) {
+    res.status(404).json({ message: "Cannot view all comments."});
+    return;
+  }
+  res.status(200).json(viewAllComments);
+ }catch (err) {
+  res.status(500).json(err);
+ }
 });
+
+
+
+
 
 router.post('/', withAuth, (req, res) => {
   // check the session
